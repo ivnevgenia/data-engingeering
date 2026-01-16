21044c99c4ecdfec2ec2cf608c437204f9fb3636
ivnevgenia <ivn.evgenia@gmail.com>
2026-01-16 07:53:24 +0000

    Добавила папку homework и все изменения

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -1,20 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-#import click
+import click
 import pandas as pd
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
-
-pg_user = 'root'
-pg_pass = 'root'
-pg_host = 'localhost'
-pg_port = 5432
-pg_db = 'ny_taxi'
-chunksize=100000
-year = 2021
-month = 1
-target_table = 'yellow_taxi_data'
 
 dtype = {
     "VendorID": "Int64",
@@ -41,29 +31,22 @@ parse_dates = [
 ]
 
 
-#@click.command()
-#@click.option('--pg-user', default='root', help='PostgreSQL user')
-#@click.option('--pg-pass', default='root', help='PostgreSQL password')
-#@click.option('--pg-host', default='localhost', help='PostgreSQL host')
-#@click.option('--pg-port', default=5432, type=int, help='PostgreSQL port')
-#@click.option('--pg-db', default='ny_taxi', help='PostgreSQL database name')
-#@click.option('--year', default=2021, type=int, help='Year of the data')
-#@click.option('--month', default=1, type=int, help='Month of the data')
-#@click.option('--target-table', default='yellow_taxi_data', help='Target table name')
-#@click.option('--chunksize', default=100000, type=int, help='Chunk size for reading CSV')
-def run():
-    pg_user = 'root'
-    pg_pass = 'root'
-    pg_host = 'localhost'
-    pg_port = 5432
-    pg_db = 'ny_taxi'
-    chunksize=100000
-    year = 2021
-    month = 1
-    target_table = 'yellow_taxi_data'
+@click.command()
+@click.option('--pg_user', default='root', help='PostgreSQL user')
+@click.option('--pg_pass', default='root', help='PostgreSQL password')
+@click.option('--pg_host', default='localhost', help='PostgreSQL host')
+@click.option('--pg_port', default=5432, type=int, help='PostgreSQL port')
+@click.option('--pg_db', default='ny_taxi', help='PostgreSQL database name')
+@click.option('--year', default=2021, type=int, help='Year of the data')
+@click.option('--month', default=1, type=int, help='Month of the data')
+@click.option('--target_table', default='yellow_taxi_data', help='Target table name')
+@click.option('--chunksize', default=100000, type=int, help='Chunk size for reading CSV')
+def run(pg_user, pg_pass, pg_host, pg_port, pg_db, year, month, target_table, chunksize):
     """Ingest NYC taxi data into PostgreSQL database."""
-    prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow'
-    url = f'{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz'
+   # prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow'
+   # url = f'{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz'
+     #prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow'
+     url = 'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2025-11.parquet'
 
     engine = create_engine(f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
 
